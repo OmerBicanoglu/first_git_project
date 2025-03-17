3eec9206e7512475bb8727fcc2b1e760bd80a112
OmerBicanoglu <156654774+OmerBicanoglu@users.noreply.github.com>
2025-03-17 15:07:50 +0000

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("Hello");
+        System.out.println("Merhaba");
+        System.out.println("Ola");
+        System.out.println("Hola");
     }
 
 
